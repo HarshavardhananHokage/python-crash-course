89aca95e5c7780167e562985c1e1965ddbede81c
Harshavardhanan <harshavardhan2990@gmail.com>
2025-09-29 19:07:03 +0530

    Basic Commit

---
+++ lists/prob_3_1_names.py
@@ -0,0 +1,5 @@
+frnds_list = ['Mango', 'Emmani', 'Prem', 'Hari']
+print(frnds_list[0])
+print(frnds_list[2])
+print(frnds_list[-1])
+print(frnds_list[-3])
---
+++ lists/prob_3_2_PersonalizedGreetings.py
@@ -0,0 +1,4 @@
+frnds_list = ['Mango', 'Emmani', 'Prem', 'Hari']
+
+for name in frnds_list:
+    print(f"Hello bitch {name}. What ya doin???")
