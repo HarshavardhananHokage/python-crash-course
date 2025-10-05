a7a38d53b6f298d5c7973a99918da3739a1adb08
Harshavardhanan <harshavardhan2990@gmail.com>
2025-10-05 12:16:58 +0530

    Learning

--- learning.py
+++ learning.py
@@ -1,1 +1,11 @@
-import this
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+for key, value in os.environ.items():
+    if key == "TEST_KEY":
+        print(f"{key}: {value}")
+
+value = os.getenv("TEST_KEY")
+print(f"TEST_KEY: {value}")
