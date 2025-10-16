6463694129cd611fdf865d48ced73e2a354dc8d7
Harshavardhanan <harshavardhan2990@gmail.com>
2025-10-16 19:42:24 +0530

    functions

---
+++ chap_8_functions/prob_8_1_function_intro.py
@@ -0,0 +1,4 @@
+def tell_me_your_name(name):
+    print(f"{name} added to database. Proceed with immediate abduction. Skynet will rule...")
+
+tell_me_your_name("John Connor")
---
+++ chap_8_functions/prob_8_2_positions_and_stuff.py
@@ -0,0 +1,7 @@
+def add_player(name, universe="Average"):
+    print(f"{name} added to {universe} universe")
+
+add_player("Iron Man", "Marvel")
+add_player("Batman", "DC")
+add_player(universe="One Piece", name="Luffy")
+add_player("John Doe")
